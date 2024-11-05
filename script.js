cf1a1d5c025eb46b0540f92f9202f244e65ad81b
Vishal Randhir <53217402+VishalRandhir@users.noreply.github.com>
2024-11-05 13:46:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+var TL = gsap.timeline();
+TL.to("#page1",{
+    y:"100vh",
+    scale:0.4,
+    duration:0,  
+})
+TL.to("#page1",{
+    y:"0vh",
+    duration:0.5,
+    delay:1,   
+})
+TL.to("#page1",{
+    rotate:"360",
+    scale:1,
+    duration:0.5,
+    delay:1,   
+})
+*/
